0ab269b7cdf86b0a8c36ae837031e271827ea09b
Ana-good <anabrandaoaraujo2@gmail.com>
2025-03-06 17:52:36 -0300

    teste feito

---
+++ src/app/components/PokemonTCGapi/PokemonTCGapi.jsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const API_KEY= "e1e36c9f-9fd6-470d-b06a-621b59346b6e"; //Chave para requisições na Api
+
+export async function getPokemonsCards(url = "https://api.pokemontcg.io/v2/cards") {
+    const response = await axios.get(url, {headers: {"X-Api-Key": API_KEY}});
+    return response.data;
+}
+
+/*
+    async function pegarAPI(promise) {
+    const responseAPI = await 
+    }
+
+    function nome(param) {
+    }
+*/
+
+/*
+    CONST objeto {
+        name: 'aNRONIO/"
+    }
+
+    objeto.name
+
+    <Funcao />
+
+    <Link> <Link /> <a>
+*/
